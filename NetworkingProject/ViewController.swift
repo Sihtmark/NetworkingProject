2a30829303097c0895b16219fb1dfb88e3d7acfd
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-11-16 17:51:43 +0300

    Initial Commit

---
+++ NetworkingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingProject
+//
+//  Created by Sergei Poluboiarinov on 2022-11-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
